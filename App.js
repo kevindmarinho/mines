0fdc471d59e806c2f541482ceec593c2c68bcb57
Kevin Marinho <kevindmarinho4321@gmail.com>
2020-06-22 17:53:41 -0400

    iniciando o projeto Mines

--- App.js
+++ App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import params from './src/params';
+import Field from './src/components/Field'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Iniciando o Mines</Text>
+        <Text style={styles.instructions}>Tamanho da Grade:
+      {params.getRowsAmount()}x{params.getColumnsAmount()}</Text>
+
+        <Field />
+        <Field opened />
+        <Field opened nearMines={1} />
+        <Field opened nearMines={2} />
+        <Field opened nearMines={3} />
+        <Field opened nearMines={4} />
+        <Field mined />
+        <Field mined opened />
+        <Field mined opened exploded />
+        <Field flagged />
+        <Field flagged opened />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
